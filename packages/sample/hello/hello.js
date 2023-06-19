42a7b0bde32ea5e236c8a73a35a5bb18682e103c
skinsmain <137105286+skinsmain@users.noreply.github.com>
2023-06-19 12:36:12 -0600

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,5 +1,13 @@
+let http = require('http')
+
 function main(args) {
     let name = args.name || 'stranger'
+    http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+      resp.on('data', function(ip) {
+      console.log("My public IP address is: " + ip);
+          return {"body": ip}
+      });
+    });
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
     return {"body": greeting}
